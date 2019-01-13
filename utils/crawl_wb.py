201a2b75bf3c3c56d4232c8484f0511dbb1dd6fe
Bluefissure <bluefissure@foxmail.com>
2019-01-13 22:57:36 +0800

    fix weibo crawler

--- utils/crawl_wb.py
+++ utils/crawl_wb.py
@@ -5,7 +5,7 @@ import os
 import codecs
 import urllib
 import base64
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append('/FFXIVBOT/')
 os.environ['DJANGO_SETTINGS_MODULE'] ='FFXIVBOT.settings'
 from FFXIVBOT import settings
 import django
